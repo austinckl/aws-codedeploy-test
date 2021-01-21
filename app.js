bafeb21817f83f22dd460d9f16d0d13c392b0e9d
austinckl <austinchankl@gmail.com>
2021-01-22 06:31:41 +0800

    file change

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!\n');
+  res.end('Hello Josh!\n');
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Hello Josh`);
+  console.log(`Hello Josh!`);
 });
